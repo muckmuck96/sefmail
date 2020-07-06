8452ef7c8388c70d73b112620fe15b588e69750f
Julian Loferer <1810653684@stud.fh-kufstein.ac.at>
2020-07-06 13:25:31 +0200

    removed receiver_name

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ module.exports.config = {
 };
 exports.mail = function(from, to, subject, content, callback) {
     if(this.config.apiToken == undefined) throw 'No API token given';
-    if(!from.includes('<') || !from.includes('>')) throw 'from field should be in this syntax: Name <Mail>';
-    if(!to.includes('<') || !to.includes('>')) throw 'to field should be in this syntax: Name <Mail>';
-    let fromSplit = from.split(' ');
+    if(!from.includes('<') || !from.includes('>')) throw 'from field should be in this syntax: Max Mustermann <max@mustermann.com>';
+    if(to.includes('<') || to.includes('>')) throw 'to field should be in this syntax: max@mustermann.com';
+    let fromSplit = from.split('<');
     let sender_name = fromSplit[0];
-    let sender_mail = fromSplit[1].substr(1, fromSplit[1].length - 1);
-    let toSplit = to.split(' ');
-    let receiver_name = toSplit[0];
-    let receiver_mail = toSplit[1].substr(1, toSplit[1].length - 1);
-    fetch.fetchUrl('https://api.mail.asss.ist/?token=' + this.config.apiToken + '&req=get&sender_name=' + sender_name + '&sender_mail=' + sender_mail + '&receiver_name=' + receiver_name + '&receiver_mail=' + receiver_mail + '&subject=' + subject + '&content=' + content, function(error, meta, body) {
+    let sender_mail = fromSplit[1].substr(0, fromSplit[1].length - 1);
+    fetch.fetchUrl('https://api.mail.asss.ist/?token=' + this.config.apiToken + '&req=get&sender_name=' + sender_name + '&sender_mail=' + sender_mail + '&receiver_mail=' + to + '&subject=' + subject + '&content=' + content, function(error, meta, body) {
         let response = JSON.parse(body.toString());
         callback(response);
     });
